5ee9eaa91b5cbf41ce3bdc9c556511a5170a3cff
Pranav <lifeispranav@gmail.com>
2025-06-14 23:53:19 +0530

    Added complete log setup

---
+++ Backend/src/logger/index.js
@@ -0,0 +1,11 @@
+const logger = require("./logger");
+const { logWithMeta, logInfo, logWarn, logError, logDebug } = require("./helpers");
+
+module.exports = {
+  logger,
+  logWithMeta,
+  logInfo,
+  logWarn,
+  logError,
+  logDebug
+};
