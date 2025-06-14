ad1bbc91f33015dbb9b5ae1c04e2c1064f87b7b4
Pranav <lifeispranav@gmail.com>
2025-06-15 02:25:30 +0530

    Feat: Created basic skeleton of our routing

---
+++ Backend/src/routes/v1/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const userRouter = require('./user.router')
+const linkRouter = require('./links.router')
+
+
+const v1Router = express.Router()
+
+v1Router.use('/users', userRouter)
+v1Router.use('/users', linkRouter)
+
+
+module.exports = v1Router
---
+++ Backend/src/routes/v1/links.router.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+
+const linkRouter = express.Router()
+
+
+
+
+
+module.exports = linkRouter
