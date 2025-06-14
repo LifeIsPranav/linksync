f99c6e2c2949303165f366f859e63cdf2b268976
Pranav <lifeispranav@gmail.com>
2025-06-15 01:06:58 +0530

    Feat: added model for users

---
+++ Backend/src/models/users.model.js
@@ -0,0 +1,95 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name Required to create a User"]
+  },
+
+  username: {
+    type: String,
+    required: [true, "Username is required to create a User"],
+    lowercase: true,
+    trim: true,
+    unique: true,
+    minlength: [4, "Username must be at least 4 characters long"],
+    match: [
+      /^[a-zA-Z0-9-]+$/,
+      "Username can only contain letters, numbers, and hyphens"
+    ]
+  },
+
+  email: {
+    type: String,
+    required: [true, "Email is required to create a User"],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [ /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Please provide a valid email address"]
+  },
+  
+  password: {
+    type: String,
+    required: [true, "Password is required to create a User"]
+  },
+
+  plan: {
+    type: String,
+    enum: ['paid', 'free'],
+    default: 'free'
+  },
+
+  title: {
+    type: String
+  },
+
+  bio: {
+    type: String
+  },
+
+  avatar: {
+    type: String
+  },
+
+  displayName: { 
+    type: String 
+  },
+
+  location: { 
+    type: String 
+  },
+
+  website: { 
+    type: String 
+  },
+
+  customDomain: { 
+    type: String,
+    unique: true
+  },
+
+  profileViews: { 
+    type: Number,
+    default: 0 
+  },
+
+  status: { 
+    type: String, 
+    enum: ['active', 'inactive'], 
+    default: 'active' 
+  },
+
+  image: [{
+    type: String
+  }],
+
+  links: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Link'
+  }]
+})
+
+
+const userModel = mongoose.model("User", userSchema)
+module.exports = userModel
