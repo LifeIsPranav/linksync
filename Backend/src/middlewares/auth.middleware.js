0e7288582b4f12710789d198a4885b17babe273f
Pranav <lifeispranav@gmail.com>
2025-06-16 00:25:13 +0530

    Added auth middleware

---
+++ Backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,20 @@
+const { JWT_SECRET_KEY } = require("../config/serverConfig");
+const Unauthorized = require("../errors/clientSide/unauthorized.error");
+const jwt = require('jsonwebtoken')
+
+const authMiddleware = (req, res, next) => {
+
+  const username = req.cookies.token || req.header('Authorization')?.replace('Bearer ', '');
+
+  if(!token) {
+    throw new Unauthorized()
+  }
+
+  const decoded = jwt.verify(token, JWT_SECRET_KEY)
+  req.userId = decoded
+  
+  next()
+}
+
+
+module.exports = authMiddleware
